38005a1e8f2c26e9bb754158c83b739d2f0bd3aa
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-06 11:23:29 -0700

    Completed inserting DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainId = document.getElementById("main")
+
+mainId.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+// document.body.append(newHeader)
+
+newHeader.textContent = "Jason is the champion"
